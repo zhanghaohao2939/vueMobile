9998d1d154a249cba3eaf3c212163b0ceb2113ac
haohao123 <zhanghaohao@vguang.cn>
2020-04-28 17:00:18 +0800

    add rem.js

---
+++ vue-mobile/src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div class="test"> 
+      张浩浩测试!!!
+    </div>
+  </div>
+</template>
+
+<style lang="less">
+  .test{
+    width: 1rem;
+    height: 1rem;
+    text-align: center;
+    line-height: 1rem;
+    border: 1px solid #ccc;
+  }
+</style>
---
+++ vue-mobile/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="test">
+      张浩浩测试!!!
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<style lang="less" scoped>
+  .test{
+    width: 2.4rem;
+    height: 2.4rem;
+    text-align: center;
+    line-height: 2.4rem;
+    border: 1px solid #ccc;
+  }
+</style>
